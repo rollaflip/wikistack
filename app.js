c01fdb453432ad4645d1425c9797e23751cc3323
iknepper <iknepper@hotmail.com>
2018-04-28 23:42:20 -0400

    semi comple as of EOD Friday

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ const sequelize = require("sequelize");
 const morgan = require("morgan");
 const layout = require("./views/layout");
 const { db, Page, User } = require('./models');
-
 const app = express();
+const wikiRouter = require('./routes/wiki');
+const userRouter = require('./routes/user');
+
+app.use('/wiki', wikiRouter);
+app.use('/user', userRouter);
+
+
+
 
 db.authenticate().
 then(() => {
   console.log('connected to the database');
 });
 
+// app.get("/", (req, res) => {
+//   res.send(layout());
+// });
+
 app.get("/", (req, res) => {
-  res.send(layout());
+  res.redirect('/wiki');
 });
 
 
@@ -34,7 +45,7 @@ const init = async () => {
   })
 }
 
-init();
+//init();
 
 
 
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req , res , next )=>{
+    res.send('got to GET /wiki/')
+}) 
+
+router.post('/',(req,res,next)=>{
+    res.send('got to POST /wiki/')
+}) 
+
+router.get('/add',(req,res,next)=>{
+    res.send('got to GET /wiki/')
+}) 
+
+module.exports = router
